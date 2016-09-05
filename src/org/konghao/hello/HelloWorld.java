1ec7457d645287495de50e32a404c59291f0118c
konghao <ynkonghao@gmail.com>
2016-09-05 22:12:00 +0800

    测试提交

--- src/org/konghao/hello/HelloWorld.java
+++ src/org/konghao/hello/HelloWorld.java
@@ -5,7 +5,7 @@ package org.konghao.hello;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world-------------");
         System.out.println("hello=====");
     }
 }
