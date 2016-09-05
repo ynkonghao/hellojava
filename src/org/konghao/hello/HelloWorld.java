a98f9356bef815b78944aec08971ce4de2d28251
konghao <ynkonghao@gmail.com>
2016-09-05 21:41:07 +0800

    第一次提交

---
+++ src/org/konghao/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.konghao.hello;
+
+/**
+ * Created by konghao on 2016/9/5.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello");
+    }
+}
