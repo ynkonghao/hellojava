e8b63f56167e76d18f1d554de62d3ddeb9b5129c
zsl131 <398986099@qq.com>
2016-09-05 21:48:48 +0800

    提交了RootApplication

---
+++ src/com/zslin/hello/RootApplication.java
@@ -0,0 +1,10 @@
+package com.zslin.hello;
+
+/**
+ * Created by zsl-pc on 2016/9/5.
+ */
+public class RootApplication {
+    public static void main(String [] args) {
+        System.out.println("Hello World");
+    }
+}
